a4a6918af2ab53f121e797cc419e2d2656842d5c
JDG07 <162812317+JDG07@users.noreply.github.com>
2026-08-05 16:45:07 +0800

    temporary route to check video upload

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,12 @@ Route::view('/about', 'about')->name('about');
 Route::view('/projects', 'projects')->name('projects');
 
 Route::view('/contact', 'contact')->name('contact');
+
+Route::get('/php-upload', function () {
+    return [
+        'upload_max_filesize' => ini_get('upload_max_filesize'),
+        'post_max_size' => ini_get('post_max_size'),
+        'max_file_uploads' => ini_get('max_file_uploads'),
+        'memory_limit' => ini_get('memory_limit'),
+    ];
+});
